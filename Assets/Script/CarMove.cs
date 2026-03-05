42aa9d0678690ccba4fde86167043a94544a5ced
Darkvdz <famparintorn@gmail.com>
2026-03-05 17:09:44 +0700

    Script-CarMove

--- Assets/Script/CarMove.cs
+++ Assets/Script/CarMove.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class CarMove : MonoBehaviour
 {
+    [SerializeField] private float speed = 5.0f;
+    Rigidbody rb;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(speed * transform.up * -1 * Time.deltaTime);
     }
 }
